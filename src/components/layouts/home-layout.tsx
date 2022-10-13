0590da9c0a185ab2c15b5abc3f9eb28d24febeb4
Aaron Pavlick <apavlick@yext.com>
2022-10-13 17:56:38 -0400

    updating components

--- src/components/layouts/home-layout.tsx
+++ src/components/layouts/home-layout.tsx
@@ -1,28 +1,40 @@
 import * as React from "react";
 import Footer from "../footer";
-import PageLayout from "./page-layout";
+import Header from "../header";
 
 interface HomeLayoutProps {
   GreetingContent?: () => JSX.Element;
-  InfoContent?: () => JSX.Element;
   firstName?: string;
   lastName?: string;
+  sections?: {
+    title: string;
+    Section: React.ReactChild;
+  }[];
 }
 
 const HomeLayout = ({
   GreetingContent,
-  InfoContent,
   firstName,
   lastName,
+  sections,
 }: HomeLayoutProps) => {
+  const [activeSection, setActiveSection] = React.useState(0);
+
   return (
-    <PageLayout>
+    <div className="relative min-h-screen font-display">
+      <Header
+        activeTab={sections?.[activeSection].title}
+        tabs={sections?.map((section) => ({
+          label: section.title,
+          onClick: () => setActiveSection(sections.indexOf(section)),
+        }))}
+      />
       <div className="top-0 flex min-h-[30vh] w-full animate-gradient bg-gradient-to-br from-gray-400 via-gray-600 to-blue-800 bg-overflow pt-[120px] pb-6 text-gray-50 md:fixed md:h-full md:w-1/2 md:items-center">
         {GreetingContent && <GreetingContent />}
       </div>
       <div className="md:absolute md:right-0 md:w-1/2 md:pt-[120px]">
         <div className="w-full pb-[145px] text-gray-800 md:min-h-[calc(100vh-152px)]">
-          {InfoContent && <InfoContent />}
+          {sections?.[activeSection].Section}
           <Footer
             footerContainerStyle="absolute bottom-0"
             firstName={firstName}
@@ -30,7 +42,7 @@ const HomeLayout = ({
           />
         </div>
       </div>
-    </PageLayout>
+    </div>
   );
 };
 
--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -8,11 +8,9 @@ import {
   GetHeadConfig,
   HeadConfig,
 } from "@yext/pages";
-import SocialLinks from "../components/social-links";
 import HomeLayout from "../components/layouts/home-layout";
-import InfoSection from "../components/info-section";
-import BioSection from "../components/bio-section";
 import Greeting from "../components/greeting";
+import PersonalInfo from "../components/personal-info";
 
 export const getPath: GetPath<TemplateProps> = () => {
   return `index.html`;
@@ -30,39 +28,36 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = (
 
 const Home: Template<TemplateRenderProps> = () => {
   return (
-    <>
-      <HomeLayout
-        GreetingContent={() => (
-          <Greeting name="Aaron" role="Developer Evangelist @ Yext" />
-        )}
-        InfoContent={() => (
-          <div className="centered-container">
-            <SocialLinks
+    <HomeLayout
+      GreetingContent={() => (
+        <Greeting name="Aaron" role="Developer Evangelist @ Yext" />
+      )}
+      sections={[
+        {
+          title: "Home",
+          Section: (
+            <PersonalInfo
               twitter="https://twitter.com/apav_dev"
               github="https://github.com/apav-dev"
-              dev_to="https://dev.to/apavlick"
-            />
-            <InfoSection title="Introduction">
-              <p className=" text-lg">
-                My name is Aaron and I&apos;m a developer evangelist at Yext.
-                This is my first Developer Relations role and I&apos;m really
+              devTo="https://dev.to/apavlick"
+              introduction="My name is Aaron and I'm a developer evangelist at Yext.
+                This is my first Developer Relations role and I'm really
                 enjoying it! We have a lot of cool, new tools for people looking
-                to build websites and I&apos;m excited to demonstrate how to use
+                to build websites and I'm excited to demonstrate how to use
                 them. Before I started working at Yext, I worked as a software
-                engineer at a bank and a healthcare company.
-              </p>
-            </InfoSection>
-            <InfoSection title="Bio">
-              <BioSection
-                home="New York, NY"
-                skills={["Yext", "React", "Typescript", "Deno"]}
-                interests={["Skiing", "Podcasts", "TV", "Music"]}
-              />
-            </InfoSection>
-          </div>
-        )}
-      />
-    </>
+                engineer at a bank and a healthcare company."
+              home="New York, NY"
+              skills={["Yext", "React", "Typescript", "Deno"]}
+              interests={["Skiing", "Podcasts", "TV", "Music"]}
+            />
+          ),
+        },
+        {
+          title: "Blogs",
+          Section: <></>,
+        },
+      ]}
+    />
   );
 };
 
