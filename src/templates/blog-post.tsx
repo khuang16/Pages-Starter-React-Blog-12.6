2106c3793b02952efd576ad1b37f668601a2be13
khuang16 <khuang@yext.com>
2022-12-06 13:58:22 -0500

    blog posts

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -13,19 +13,35 @@ import { Image } from "@yext/pages/components";
 import InfoSection from "../components/info-section";
 import { formatDate, renderBlogContent } from "../util";
 
+export const config: TemplateConfig = {
+  stream: {
+    $id: "blog",
+    filter: {
+      entityTypes: ["ce_blog"],
+    },
+    fields: ["id", "name", "datePosted", "body", "slug", "c_coverPhoto"],
+    localization: {
+      locales: ["en"],
+      primary: false,
+    },
+  },
+};
+
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  return "blog";
+  return document.slug;
 };
 
 const BlogPost: Template<TemplateRenderProps> = ({
   document,
 }: TemplateProps) => {
+
   return (
     <>
       <div className="mx-auto flex w-full max-w-4xl flex-col items-start justify-center">
-        <InfoSection titleCssStyles="text-5xl pb-4" title={"BLOG TITLE"}>
-          <p className="font-semibold">{"INSERT DATE"}</p>
-          <div className="font-display">{"INSERT BLOG CONTENT"}</div>
+				<InfoSection titleCssStyles="text-5xl pb-4" title={document.name}>
+        {document.c_coverPhoto && <Image image={document.c_coverPhoto} />}
+          <p className="font-semibold">{document.datePosted}</p>
+          <div className="font-display">{renderBlogContent(document.body)}</div>
         </InfoSection>
       </div>
     </>
